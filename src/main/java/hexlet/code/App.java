698ef01ea4a1f8efb7fb8f070586568024f25f53
Roman <ramzesnic@gmail.com>
2021-10-14 15:26:56 +0300

    javalin configure

---
+++ src/main/java/hexlet/code/App.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public final class App {
+    private static final String defaultPort = "5000";
+
+    private static int getPort() {
+        final String port = System.getenv().getOrDefault("PORT", defaultPort);
+        return Integer.valueOf(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+
+    public static Javalin getApp() {
+        final Javalin app = Javalin.create(config -> {
+            config.enableDevLogging();
+        });
+        addRoutes(app);
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
